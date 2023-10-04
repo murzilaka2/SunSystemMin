df7102b4508b747db223f3e2c5b2c851c66be9f4
Leonid <enykoruna1@gmail.com>
2023-10-04 11:39:33 +0300

    Add project files.

---
+++ SunSystemMin/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SunSystemMin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(870, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(961, 25);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(528, 25);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.Size = new System.Drawing.Size(304, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(662, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Speed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1412, 972);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SunSystem";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SunSystemMin/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace SunSystemMin
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly int px, py, dst;
+        public Form1()
+        {
+            InitializeComponent();
+            px = Width / 2;
+            py = Height / 2;
+            dst = 100;
+        }
+
+        //Velocity - скорость движения
+        float mercuryAngle = 0, mercuryVelocity = 1, venusAngle = 0, venusVelocity = 0.5f, earthAngle = 0, earthVelocity = 0.25f, 
+            marsAngle = 0, marsVelocity = 0.25f;
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            StringFormat str = new StringFormat();
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.TranslateTransform(px, py, MatrixOrder.Append);
+
+            //Draw Mercury`s orbit
+            g.DrawEllipse(Pens.White, -dst / 2 + 6, -dst / 2 + 6, 100, 100);
+
+            //Draw Venus`s orbit
+            g.DrawEllipse(Pens.White, -dst + 6, -dst + 6, 200, 200);
+
+            //Draw Earth`s orbit
+            g.DrawEllipse(Pens.White, -2 * dst + 6, -2 * dst + 6, 400, 400);
+
+            //Draw Mars orbit
+            g.DrawEllipse(Pens.White, -2 * dst - 43, -3 * dst + 60, 500, 500);
+
+            g.ResetTransform();
+
+            //Draw Sun
+            g.FillEllipse(Brushes.Yellow, px - 12, py - 6, 40, 40);
+            g.DrawString("Sun", Font, new SolidBrush(Color.Yellow), px - dst + 95, py - dst + 75, str);
+
+            //Draw Mercury
+            int mercuryX = (int)(px + dst / 2 * Math.Sin(mercuryAngle * Math.PI / 182.5f));
+            int mercuryY = (int)(py + dst / 2 * Math.Cos(mercuryAngle * Math.PI / 182.5f));
+            g.FillEllipse(Brushes.Gray, mercuryX, mercuryY, 15, 15);
+            mercuryAngle -= mercuryVelocity;
+
+            //Draw Venus
+            int venusX = (int)(px + dst * Math.Sin(venusAngle * Math.PI / 182.5f));
+            int venusY = (int)(py + dst * Math.Cos(venusAngle * Math.PI / 182.5f));
+            g.FillEllipse(Brushes.OrangeRed, venusX - 5, venusY - 5, 20, 20);
+            venusAngle -= venusVelocity;
+
+            //Draw Earth
+            int earthX = (int)(px + 2 * dst * Math.Sin(earthAngle * Math.PI / 182.5f));
+            int earthY = (int)(py + 2 * dst * Math.Cos(earthAngle * Math.PI / 182.5f));
+            g.FillEllipse(Brushes.Aqua, earthX - 5, earthY - 5, 20, 20);            
+            earthAngle -= earthVelocity;
+
+            //Draw Moon`s orbit
+            g.DrawEllipse(Pens.White, earthX - 25, earthY - 25, 60, 60);
+
+            //Draw Moon
+            int moonX = (int)(earthX + 30 * Math.Sin(earthAngle * Math.PI / 30f));
+            int moonY = (int)(earthY + 30 * Math.Cos(earthAngle * Math.PI / 30f));
+            g.FillEllipse(Brushes.LightGray, moonX, moonY, 10, 10);
+
+            //Draw Mars
+            int marsX = (int)(px + 2.5 * dst * Math.Sin(marsAngle * Math.PI / 182.5f));
+            int marsY = (int)(py + 2.5 * dst * Math.Cos(marsAngle * Math.PI / 182.5f));
+            g.FillEllipse(Brushes.Brown, marsX, marsY, 20, 20);
+            marsAngle -= marsVelocity;
+        }
+    }
+}
